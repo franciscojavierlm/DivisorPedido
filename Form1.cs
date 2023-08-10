d4b2c74d318eb0a96e3560143a9fbca80764dcfa
francisco javier lópez morales <franciscojavier_lopez@ucol.mx>
2023-08-09 20:03:27 -0600

    PRUEBA

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,47 @@ namespace DIVISOR_PEDIDOS
         {
             InitializeComponent();
         }
+
+        List<pedidoDetalle> pedidoD = new List<pedidoDetalle>();
+        List<pedidoDetalle> pedidoD2 = new List<pedidoDetalle>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 100, clave_articulo = "01", sector = 1 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 101, clave_articulo = "02", sector = 1 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 102, clave_articulo = "03", sector = 2 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 103, clave_articulo = "04", sector = 3 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 104, clave_articulo = "05", sector = 3 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 105, clave_articulo = "06", sector = 4 });
+            pedidoD.Add(new pedidoDetalle() { docto_ve_id = 106, clave_articulo = "07", sector = 2 });          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            separaSectorUno();
+        }
+
+        private void separaSectorUno()
+        {
+  
+            pedidoD2 = BuscaSector(pedidoD, 3).ToList();
+
+            pedidoD.RemoveAll(sector =>
+            {
+                if (sector.sector == 3)
+                    return true;
+                else
+                    return false;
+            });
+        }
+
+        private IEnumerable<pedidoDetalle> BuscaSector(List<pedidoDetalle> pD,int sector)
+        {
+            var items = from x in pD
+                        where x.sector == sector
+                        select x;
+            return items;
+        }
+
     }
 }
---
+++ pedidoDetalle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIVISOR_PEDIDOS
+{
+    public class pedidoDetalle
+    {
+        public int docto_ve_id { get; set; }
+        public string clave_articulo { get; set; }
+
+        public int sector { get; set; }
+
+    }
+}
